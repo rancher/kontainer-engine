534d68f175ccfd20a5fe89279c7f4614ec4ce27b
Nathaniel Jenan <nathanieljenan@Nathaniels-MBP.tempe.rancherlabs.com>
2018-01-04 13:03:54 -0700

    WIP checkpoint of current state of aks driver

--- driver/aks/aks_driver.go
+++ driver/aks/aks_driver.go
@@ -3,8 +3,16 @@ package aks
 import (
 	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/containerservice/mgmt/containerservice"
+	// "github.com/Azure/azure-sdk-for-go/profiles/latest/containerservice/mgmt/containerservice"
+	// "github.com/Azure/azure-sdk-for-go/services/containerservice/mgmt/2017-09-30/containerservice"
+
 	generic "github.com/rancher/kontainer-engine/driver"
+	"fmt"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/utils"
+	"github.com/Azure/azure-sdk-for-go/arm/containerservice"
+	"github.com/Azure/go-autorest/autorest/to"
+	"os"
 )
 
 type Driver struct {
@@ -115,7 +123,7 @@ func (d *Driver) SetDriverOptions(driverOptions *generic.DriverOptions) error {
 	d.SubscriptionID = generic.GetValueFromDriverOptions(driverOptions, generic.StringType, "subscription-id").(string)
 	d.ResourceGroup = generic.GetValueFromDriverOptions(driverOptions, generic.StringType, "resource-group").(string)
 	d.AgentPoolFQDN = generic.GetValueFromDriverOptions(driverOptions, generic.StringType, "node-pool-fqdn").(string)
-	tagValues := generic.GetValueFromDriverOptions(driverOptions, generic.StringType).(generic.StringSlice)
+	tagValues := generic.GetValueFromDriverOptions(driverOptions, generic.StringSliceType).(*generic.StringSlice)
 	for _, part := range tagValues.Value {
 		kv := strings.Split(part, "=")
 		if len(kv) == 2 {
@@ -128,25 +136,74 @@ func (d *Driver) SetDriverOptions(driverOptions *generic.DriverOptions) error {
 // Create implements driver interface
 func (d *Driver) Create() error {
 	// todo: implement
-	client := containerservice.NewManagedClustersClient(d.SubscriptionID)
+	fmt.Println("Hello Azure")
+
+	subscriptionId := os.Getenv("AZURE_SUBSCRIPTION_ID")
+
+	authorizer, err := utils.GetAuthorizer(azure.PublicCloud)
+
+	if err != nil {
+		return err
+	}
+
+	client := containerservice.NewContainerServicesClient(subscriptionId)
+	client.Authorizer = authorizer
+	resultChan, errChan := client.CreateOrUpdate("kube", "go-sdk-kube-cluster-4", containerservice.ContainerService{
+		Location: to.StringPtr("eastus"),
+		Properties: &containerservice.Properties{
+			MasterProfile: &containerservice.MasterProfile{
+				DNSPrefix: to.StringPtr("kube-master-5u48932u543758473598"),
+			},
+			AgentPoolProfiles: &[]containerservice.AgentPoolProfile{
+				{
+					Name:      to.StringPtr("my-kube-agent-pool-4"),
+					DNSPrefix: to.StringPtr("kube-agent-574390817598479584749"),
+				},
+			},
+			LinuxProfile: &containerservice.LinuxProfile{
+				AdminUsername: to.StringPtr("adminschmadmin2"),
+				SSH: &containerservice.SSHConfiguration{
+					PublicKeys: &[]containerservice.SSHPublicKey{
+						{
+							KeyData: to.StringPtr("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGYwRDsL7LQ4NUSYfzT0nx/aVXNTx5HpgnYjQ9d4OT576JmSDoddQm1HSoqXXIqJxvCGfHmiLOUpR9yNWB57t5t6Hi/x3izp0qBB7XS0SQYzdScw7n8W1AuNzv1pi6kbIGe08IJBv2TbPvpH3GZRcb4uk5pAjQKyeGPww77hN6NqFHogrosRSpLvHNMNZXwKlg3M0PSMdDzPpBTVPW2Sh+06D+tp7LK31WaPUYhAU6jkQY/c6t3O0UCm+t+wwrD09znyKS1fpUDMrnTmNbE9hZ8Bo5X0TnuLc3J6dligr1539Of0ejhzKwpkciv66u+tB2z+udyaLk5sN9qa00oPGj nathanieljenan@MacBook-Pro.tempe.rancherlabs.com\n"),
+						},
+					},
+				},
+			},
+		},
+	}, nil)
+
+	err = <-errChan
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(<-resultChan)
+
+	return nil
 }
 
 // Update implements driver interface
 func (d *Driver) Update() error {
 	// todo: implement
+	return nil
 }
 
 // Get implements driver interface
 func (d *Driver) Get() (*generic.ClusterInfo, error) {
 	// todo: implement
+	return nil, nil
 }
 
 func (d *Driver) PostCheck() error {
 	// todo: implement
+	return nil
 }
 
 // Remove implements driver interface
 func (d *Driver) Remove() error {
-	// todo: implement	
+	// todo: implement
+	return nil
 
 }
