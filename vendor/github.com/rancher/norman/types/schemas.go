2c4c54ec9921ec76f4f694dedb2cd957943babb9
Nathan Jenan <nathanj@rancher.com>
2018-10-02 09:11:45 -0700

    Update Vendor

--- vendor/github.com/rancher/norman/types/schemas.go
+++ vendor/github.com/rancher/norman/types/schemas.go
@@ -86,9 +86,6 @@ func (s *Schemas) doRemoveSchema(schema Schema) *Schemas {
 }
 
 func (s *Schemas) removeReferences(schema *Schema) {
-	fullType := convert.ToFullReference(schema.Version.Path, schema.ID)
-	delete(s.references, fullType)
-
 	for name, values := range s.references {
 		changed := false
 		var modified []BackReference
@@ -173,7 +170,12 @@ func (s *Schemas) embed(schema *Schema) {
 	newSchema.ResourceFields = map[string]Field{}
 
 	for k, v := range target.ResourceFields {
-		newSchema.ResourceFields[k] = v
+		// We remove the dynamic fields off the existing schema in case
+		// they've been removed from the dynamic schema so they won't
+		// be accidentally left over
+		if !v.DynamicField {
+			newSchema.ResourceFields[k] = v
+		}
 	}
 	for k, v := range schema.ResourceFields {
 		newSchema.ResourceFields[k] = v
@@ -343,7 +345,28 @@ type MultiErrors struct {
 	Errors []error
 }
 
-func NewErrors(errors ...error) error {
+type Errors struct {
+	errors []error
+}
+
+func (e *Errors) Add(err error) {
+	if err != nil {
+		e.errors = append(e.errors, err)
+	}
+}
+
+func (e *Errors) Err() error {
+	return NewErrors(e.errors...)
+}
+
+func NewErrors(inErrors ...error) error {
+	var errors []error
+	for _, err := range inErrors {
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+
 	if len(errors) == 0 {
 		return nil
 	} else if len(errors) == 1 {
