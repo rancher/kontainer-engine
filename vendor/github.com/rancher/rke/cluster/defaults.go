e8375f5471d7b5b6d54c784fdf5b1597729070b7
moelsayed <m.elsayed@gmail.com>
2019-04-02 13:02:23 +0200

    vendor update

--- vendor/github.com/rancher/rke/cluster/defaults.go
+++ vendor/github.com/rancher/rke/cluster/defaults.go
@@ -150,7 +150,8 @@ func (c *Cluster) setClusterDefaults(ctx context.Context) error {
 		return err
 	}
 
-	if len(c.DNS.Provider) == 0 {
+	if c.DNS == nil || len(c.DNS.Provider) == 0 {
+		c.DNS = &v3.DNSConfig{}
 		c.DNS.Provider = DefaultDNSProvider
 	}
 
@@ -201,7 +202,9 @@ func (c *Cluster) setClusterServicesDefaults() {
 		c.Services.Etcd.ExtraArgs[DefaultEtcdHeartbeatIntervalName] = DefaultEtcdHeartbeatIntervalValue
 	}
 
-	if c.Services.Etcd.BackupConfig != nil && c.Services.Etcd.BackupConfig.Enabled != nil && *c.Services.Etcd.BackupConfig.Enabled {
+	if c.Services.Etcd.BackupConfig != nil &&
+		(c.Services.Etcd.BackupConfig.Enabled == nil ||
+			(c.Services.Etcd.BackupConfig.Enabled != nil && *c.Services.Etcd.BackupConfig.Enabled)) {
 		if c.Services.Etcd.BackupConfig.IntervalHours == 0 {
 			c.Services.Etcd.BackupConfig.IntervalHours = DefaultEtcdBackupConfigIntervalHours
 		}
